16a0982db2a587704465c62d6d31f86c0e6ff087
justdoitindark nothing <workingnnn6@gmail.com>
2023-03-23 01:24:11 +0000

    [clear comnit]

---
+++ bluesea_ver.c
@@ -0,0 +1,15 @@
+/* bluesea_version.c : /sys/module/bluesea/version
+* -----------------&-------------------
+/
+/
+/                 4.9                 
+/
+/
+/--------------------------------------*/
+ 
+
+#include <iostream>
+#include <stdio.h>
+
+int display_text(){
+         
